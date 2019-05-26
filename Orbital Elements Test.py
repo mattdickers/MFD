0b20fb082974caae8b870fcdd52fd19f0cadc3e5
mattdickers <mattdickers@gmail.com>
2019-05-26 20:29:50 +0100

    Updated latitute/longitude calculations. Made Map Plotter 2 file for testing.
    Updated the lat/lon calculations in Orbital Elements Test to not use wernher; these values still need to be verified.
    
    Made a testing file for these to be displayed in. Seems to not display them correctly.

--- Orbital Elements Test.py
+++ Orbital Elements Test.py
@@ -9,7 +9,7 @@ orbit = vessel.orbit
 G = 6.67408e-11
 π = np.pi
 deg = np.pi/180
-μ = G*vessel.orbit.body.mass
+μ = vessel.orbit.body.gravitational_parameter
 
 t0 = ksc.ut
 i = orbit.inclination
@@ -18,7 +18,7 @@ i = orbit.inclination
 e = orbit.eccentricity
 a = orbit.semi_major_axis
 M0 = orbit.mean_anomaly_at_epoch
-V = orbit.true_anomaly/deg
+V0 = orbit.true_anomaly
 epoch = orbit.epoch
 period = orbit.period
 
@@ -58,9 +58,9 @@ def EccentricAnomalyIterate():
     return E
 
 def ConvertToCartesian():
-    q = np.array([[a*np.cos(E-e)],
+    q = np.array([[a*(np.cos(E)-e)],
                  [a*np.sqrt(1-e**2)*np.sin(E)],
-                  [0]])
+                  [0]],dtype=np.float64)
     qx = q[0][0]
     qy = q[1][0]
 
@@ -81,9 +81,37 @@ def ConvertToCartesian():
                    [0, 0, 1]])
 
     xyzMatrix = np.matmul(np.matmul(R_Ω,R_i),R_ω)
-    print(np.matmul(q,xyzMatrix)) #TODO need to fix fact that q is an array of arrays, and hence cannot be multiplied
+    #q1 = [q[0][0].tolist(),q[1][0].tolist(),q[2].tolist()]
+    print(np.matmul(q,xyzMatrix)) #TODO need to fix fact that q is an array of arrays, and hence cannot be multiplied -> maybe set q datatype to float, and make z values be an aray of all 0s
 
-M1 = M0 + np.sqrt((μ)/(a**3))*(dt-epoch)
-EccentricAnomalyIterate()
-ConvertToCartesian()
+def GetLatLon():
+    M1 = M0 + np.sqrt((μ) / (a ** 3)) * (dt - epoch)
 
+    maxIter = 15
+    maxError = 1e-11
+
+    count = 0
+    if e<0.8:
+        E = M1
+    F = E - e * np.sin(M1) - M1
+    while np.all((np.abs(F) > maxError)) and np.all(count < maxIter):
+        E = E - F / (1 - e * np.cos(E))
+        F = E - e * np.sin(E) - M1
+        count += 1
+
+    V1 = 2*np.arctan(np.sqrt((1+e)/(1-e))*np.tan(E/2))
+    r = (a*(1-e**2))/(1+e*np.cos(V1))
+
+    position = r*np.array([[np.cos(Ω)*np.cos(ω+V1) - np.sin(Ω)*np.sin(ω+V1)*np.cos(i)],
+                           [np.sin(Ω)*np.cos(ω+V1) + np.cos(Ω)*np.sin(ω+V1)*np.cos(i)],
+                           [np.sin(ω+V1)*np.sin(i)]])
+
+    lat = np.sin(position[2]/r)/deg
+    lon = np.arctan2(position[1],position[0])/deg
+    return lat, lon #TODO check how logical these values are
+
+#M1 = M0 + np.sqrt((μ)/(a**3))*(dt-epoch)
+#EccentricAnomalyIterate()
+#ConvertToCartesian2()
+lat, lon = GetLatLon()
+print(lon)
